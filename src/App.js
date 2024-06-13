b1db7c298a44772d8ec7fe5339363c1ded961930
xalexkm <xalexkm@gmail.com>
2024-06-13 23:26:57 +0100

    feat: add filtering by file parameters

--- src/App.js
+++ src/App.js
@@ -14,26 +14,42 @@ function App() {
     setView(files);
   }
 
+  const handleSort = (attr) => {
+    const sortedView = [...view].sort((a, b) => {
+      const aValue = a[attr];
+      const bValue = b[attr];
+
+      if (aValue !== undefined && bValue !== undefined) {
+        return aValue.localeCompare(bValue);
+      }
+
+      if (aValue === undefined) return 1;
+      if (bValue === undefined) return -1;
+
+      return 0;
+    });
+
+    setView(sortedView);
+  };
+
   return (
     <div className="wrapper">
       <Button onClick={() => setView(route.pop())}>Back</Button>
       <Table striped bordered hover>
         <thead>
         <tr>
-          <th>Type</th>
-          <th>Name</th>
-          <th>Added</th>
+          <th onClick={() => handleSort('type')}>Type</th>
+          <th onClick={() => handleSort('name')}>Name</th>
+          <th onClick={() => handleSort('added')}>Added</th>
         </tr>
         </thead>
         <tbody>
           {
-            view.map((item) => <>
-              <tr>
-                <td>{item.type === 'folder' ? <Button onClick={() => handleFolder(item.files)}>Folder</Button> : item.type }</td>
-                <td>{item.name}</td>
-                <td>{item.added}</td>
-              </tr>
-            </> )
+            view.map((item) => <tr key={item.name}>
+              <td>{item.type === 'folder' ? <Button onClick={() => handleFolder(item.files)}>Folder</Button> : item.type }</td>
+              <td>{item.name}</td>
+              <td>{item.added}</td>
+            </tr> )
           }
         </tbody>
       </Table>
