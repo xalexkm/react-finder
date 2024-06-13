1755a2c69d7bb418c9e5cd7d56019370824a017a
xalexkm <xalexkm@gmail.com>
2024-06-13 23:47:31 +0100

    wip: add filtering by name

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
 import {useState} from "react";
 import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
 
 function App() {
   const [view, setView] = useState(dataset);
   const [route] = useState([]);
+  const [filteredView, setFilteredView] = useState(dataset);
 
   const handleFolder = (files) => {
     route.push(view);
@@ -32,16 +34,26 @@ function App() {
     setView(sortedView);
   };
 
+  const handleInput = (input) => {
+    if (input !== '') {
+      const filtered = view.filter((item) => item.name.toLowerCase().startsWith(input.toLowerCase()));
+      setFilteredView(filtered);
+    } else {
+      setFilteredView(view);
+    }
+  };
+
   return (
     <div className="wrapper">
       <Button onClick={() => setView(route.pop())}>Back</Button>
+      <Form.Control onChange={(e) => handleInput(e.target.value)} size="lg" type="text" placeholder="Large text" />
       <Table striped bordered hover>
         <thead>
-        <tr className="header">
-          <th onClick={() => handleSort('type')}>Type</th>
-          <th onClick={() => handleSort('name')}>Name</th>
-          <th onClick={() => handleSort('added')}>Added</th>
-        </tr>
+          <tr className="header">
+            <th onClick={() => handleSort('type')}>Type</th>
+            <th onClick={() => handleSort('name')}>Name</th>
+            <th onClick={() => handleSort('added')}>Added</th>
+          </tr>
         </thead>
         <tbody>
           {
